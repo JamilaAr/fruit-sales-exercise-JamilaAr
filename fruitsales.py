7f648f72e7c3e6a144803ce28ddb8f486c535ab2
jamila <jamilaarabat93@gmail.com>
2024-09-07 09:52:16 -0400

    Pandas DataFrame created with fuit sales data

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,28 @@
-# add your code here
+import pandas as pd
+
+#create DataFrame with fruits sales information
+
+data ={'Apple' : [35, 41],
+       'Bananas' : [21, 34]
+}
+
+#Define the index of DataFrame
+
+index = ['2017 Sales', '2018 Sales']
+
+#create the DataFrame
+df = pd.DataFrame(data, index)
+
+#print the DataFrame
+print(df)
+
+#write the data to a CSV file
+ 
+df.to_csv('fruit.csv')
+
+
+print("the data has been printed to 'fruit.csv' file")
+
+
+
+
